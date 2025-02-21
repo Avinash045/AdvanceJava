c54542b40c040ec08133489577f70e6bfb4d13f8
Avinash Bhalerao <164344545+Avinash045@users.noreply.github.com>
2025-02-21 12:40:46 +0530

    Add files via upload

---
+++ StudentManagement/src/main/java/com/example/StudEntity/Student.java
@@ -0,0 +1,42 @@
+package com.example.StudEntity;
+
+public class Student {
+	
+	private long id;
+	
+	private String Name;
+	
+	private int RollCall;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+	
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public int getRollCall() {
+		return RollCall;
+	}
+
+	public void setRollCall(int rollCall) {
+		RollCall = rollCall;
+	}
+
+	public Student(long id, String name, int rollCall) {
+		super();
+		this.id = id;
+		Name = name;
+		RollCall = rollCall;
+	}
+	
+}
